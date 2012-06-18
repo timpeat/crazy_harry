ee0dd9ad8a98a4b9224013f6ddee7825370fddc3
TA Tyree <tatyree@gmail.com>
2012-06-18 14:48:08 +0100

    Redact fragments, remove tags as needed.

--- lib/crazy_harry/redactor.rb
+++ lib/crazy_harry/redactor.rb
@@ -1,5 +1,62 @@
 module CrazyHarry
   class Redactor
+    class InvalidStripMethod < StandardError; end;
+
+    STRIP_METHODS =  %w(strip prune escape whitewash)
+
+    attr_accessor :fragment, :steps, :unsafe, :tags,
+      :text, :scope
+
+    def initialize(opts = {})
+      self.fragment = Loofah.fragment(opts.delete(:fragment)) if opts.has_key?(:fragment)
+      self.steps = []
+    end
+
+    def strip!
+      fragment.to_text
+    end
+
+    def strip(opts = {})
+      self.unsafe = opts.delete(:unsafe) || opts == {}
+      self.tags   = [opts.delete(:tags)].compact.flatten
+      self.text  =  opts.delete(:text)
+      self.scope =  opts.delete(:scope)
+
+      self.steps << strip_unsafe  if self.unsafe
+      self.steps << strip_tags    unless self.tags == []
+
+      self
+    end
+
+    def run!
+      steps.compact.each{ |step| fragment.scrub!(step) }
+      fragment.to_s.squeeze(' ').strip
+    end
+
+    private
+
+    def alter_this_node?(node)
+      ( self.text   ? node.text == self.text          : true ) &&
+      ( self.scope  ? node.parent.name == self.scope  : true ) &&
+      self.tags.include?(node.name)
+    end
+
+    def strip_unsafe
+      fail CrazyHarry::Redactor::InvalidStripMethod, "vaild methods are #{STRIP_METHODS.join(', ')}." unless valid_strip_method?
+
+      STRIP_METHODS.include?(self.unsafe.to_s) ? self.unsafe.to_sym : :prune
+    end
+
+    def valid_strip_method?
+      return true if self.unsafe == true || STRIP_METHODS.include?(self.unsafe.to_s)
+    end
+
+    def strip_tags
+      Loofah::Scrubber.new do |node|
+        content = "#{node.content} "
+        node.replace(content) if alter_this_node?(node)
+      end
+    end
 
   end
 end
--- spec/lib/crazy_harry_redactor_spec.rb
+++ spec/lib/crazy_harry_redactor_spec.rb
@@ -15,5 +15,71 @@ describe CrazyHarry::Redactor do
 
   subject { CrazyHarry::Redactor }
 
+  it "should be able to strip all tags" do
+    subject.new( fragment: "<b>a</b>\nb<br /><h3>c</h3><img src='blah.jpg' />" ).strip!.should == "a\nb\nc\n"
+  end
 
-end
+  it "should be able to strip unsafe tags" do
+    subject.new( fragment: 'Me steal <script>Cookie!</script>').strip( unsafe: true ).run!.should == 'Me steal'
+    subject.new( fragment: 'What is that <bloog>thing</bloog>?').strip( unsafe: true ).run!.should == 'What is that ?'
+  end
+
+  it "should allow the unsafe strip method to be overridden (it prunes by default)" do
+    fragment = 'Me steal <script>Cookie!</script>'
+    subject.new( fragment: fragment).strip( unsafe: :escape ).run!.should == 'Me steal &lt;script&gt;Cookie!&lt;/script&gt;'
+    subject.new( fragment: fragment).strip( unsafe: :strip ).run!.should == 'Me steal Cookie!'
+  end
+
+  it "should prune unsafe tags if called without any arguments" do
+    subject.new( fragment: 'Me steal <script>Cookie!</script>').strip.run!.should == 'Me steal'
+  end
+
+  it "should raise an error if asked for an unknown strip method" do
+    -> { subject.new( fragment: 'Any old thing').strip( unsafe: 'magical_magic' ).run! }.should raise_error(CrazyHarry::Redactor::InvalidStripMethod)
+  end
+
+  it "should be able to strip a specific tag" do
+    subject.new( fragment: '<b>Location:</b><p>Saigon</p>' ).strip( tags: 'b' ).run!.should == "Location: <p>Saigon</p>"
+  end
+
+  it "should strip every occurrence of a tag" do
+    subject.new( fragment: '<b>Location:</b><p>Saigon</p><b>Rates:</b>' ).strip( tags: 'b' ).run!.should == "Location: <p>Saigon</p>Rates:"
+  end
+
+  it "should be able to strip multiple tags" do
+    subject.new( fragment: '<b>One</b><p>Saigon <em>Plaza</em></p>').strip( tags: %w(b em)).run!.should == "One <p>Saigon Plaza </p>"
+  end
+
+  it "should close a paragraph tag early if the fragment has an illegally nested header tag" do
+    subject.new( fragment: '<b>One</b><p>Saigon <h3>Plaza</h3></p>').strip( tags: %w(b h3)).run!.should == "One <p>Saigon </p>Plaza"
+  end
+
+  context "whitespace and breaks" do
+
+    it "shouldn't add excessive extra whitespace" do
+      subject.new( fragment: '<b>Location: </b> <b>Prices:</b>' ).strip( tags: 'b' ).run!.should == 'Location: Prices:'
+    end
+
+  end
+
+  context "targeting and scope" do
+
+    it "should allow strip to be targeted by the tag content" do
+      subject.new( fragment: '<h3>Location:</h3><h3>Ho Chi Minh City</h3>' ).strip( tags: 'h3', text: 'Location:' ).run!.should ==
+        'Location: <h3>Ho Chi Minh City</h3>'
+    end
+
+    it "should be able to scope changes to specific blocks" do
+      subject.new( fragment: '<div><b>Hotels</b></div><p><b>Hotels</b></p><b>Tents</b>' ).strip( tags: 'b', scope: 'p' ).run!.should ==
+        '<div><b>Hotels</b></div><p>Hotels </p><b>Tents</b>'
+    end
+
+    it "should allow strip to be targeted and scoped" do
+      subject.new( fragment: '<b>Location:</b><p><b>Big</b> <b>Bad</b> Hotel</p>' ).strip( tags: 'b', scope: 'p', text: 'Big' ).run!.should ==
+        '<b>Location:</b><p>Big <b>Bad</b> Hotel</p>'
+    end
+
+
+  end
+
+ end
