dd6e0c4d31043904d7d09b58ed4378a442c492e6
TA Tyree <tatyree@gmail.com>
2012-06-19 12:41:38 +0100

    Refactor easiest methods to base.

---
+++ lib/crazy_harry/base.rb
@@ -0,0 +1,24 @@
+module CrazyHarry
+  class Base
+
+    attr_accessor :fragment, :scope, :steps, :text
+
+    def initialize(opts = {})
+      self.fragment = Loofah.fragment(opts.delete(:fragment)) if opts.has_key?(:fragment)
+      self.steps = []
+    end
+
+    def run!
+      steps.compact.each{ |step| fragment.scrub!(step) }
+      fragment.to_s.squeeze(' ').strip
+    end
+
+    private
+
+    def alter_this_node?(node)
+      ( self.text       ? node.text == self.text                          : true ) &&
+      ( self.scope      ? node.parent.name == self.scope                  : true )
+    end
+
+  end
+end
