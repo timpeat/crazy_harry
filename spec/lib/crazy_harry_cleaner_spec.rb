d7a85044ba4f9ca3d2433d7153fc4683fd0e9513
TA Tyree <tatyree@gmail.com>
2012-06-19 12:41:17 +0100

    Remove code-smell 'subject'

--- spec/lib/crazy_harry_cleaner_spec.rb
+++ spec/lib/crazy_harry_cleaner_spec.rb
@@ -13,42 +13,42 @@ describe CrazyHarry::Cleaner do
 
   context "change" do
 
-    subject { CrazyHarry::Cleaner }
+    let(:cleaner){ CrazyHarry::Cleaner }
 
     it "should be able to change one tag to another" do
-      subject.new( fragment: '<b>Location:</b>' ).change( from: 'b', to: 'h3' ).run!.should == '<h3>Location:</h3>'
+      cleaner.new( fragment: '<b>Location:</b>' ).change( from: 'b', to: 'h3' ).run!.should == '<h3>Location:</h3>'
     end
 
     it "should be able to change all the tags it finds" do
-      subject.new( fragment: '<b>1</b>, <b>2</b>, <b>3</b>' ).change( from: 'b', to: 'h3' ).run!.should == '<h3>1</h3>, <h3>2</h3>, <h3>3</h3>'
+      cleaner.new( fragment: '<b>1</b>, <b>2</b>, <b>3</b>' ).change( from: 'b', to: 'h3' ).run!.should == '<h3>1</h3>, <h3>2</h3>, <h3>3</h3>'
     end
 
     it "should be able to change a span tag to a div" do
-      subject.new( fragment: '<span>Hotel Details</span>' ).change( from: 'span', to: 'div' ).run!.should == '<div>Hotel Details</div>'
+      cleaner.new( fragment: '<span>Hotel Details</span>' ).change( from: 'span', to: 'div' ).run!.should == '<div>Hotel Details</div>'
     end
 
     it "should not change things that it isn't directed to" do
-      subject.new( fragment: '<h3>Hotel Details</h3>' ).change( from: 'span', to: 'div' ).run!.should == '<h3>Hotel Details</h3>'
+      cleaner.new( fragment: '<h3>Hotel Details</h3>' ).change( from: 'span', to: 'div' ).run!.should == '<h3>Hotel Details</h3>'
     end
 
     it "shouldn't choke if it gets invalid tags" do
-      subject.new( fragment: '<h3>Hotel Details</h3>' ).change( from: 'snoogenflozen', to: 'snarglebat' ).run!.should == '<h3>Hotel Details</h3>'
+      cleaner.new( fragment: '<h3>Hotel Details</h3>' ).change( from: 'snoogenflozen', to: 'snarglebat' ).run!.should == '<h3>Hotel Details</h3>'
     end
 
     context "targeting and scoping" do
 
       it "should change all occurrences" do
-       subject.new( fragment: '<div><b>Hotels</b></div><p><b>Hotels</b></p><b>Tents</b><div><p><b>Campervan</b></p></div>' ).change( from: 'b', to: 'em').run!.should ==
+       cleaner.new( fragment: '<div><b>Hotels</b></div><p><b>Hotels</b></p><b>Tents</b><div><p><b>Campervan</b></p></div>' ).change( from: 'b', to: 'em').run!.should ==
           '<div><em>Hotels</em></div><p><em>Hotels</em></p><em>Tents</em><div><p><em>Campervan</em></p></div>'
       end
 
       it "should be able to target changes by content" do
-        subject.new( fragment: 'Hot <b>hotels</b> in <b>Saigon</b>' ).change( from: 'b', to: 'em', text: 'hotels' ).run!.should ==
+        cleaner.new( fragment: 'Hot <b>hotels</b> in <b>Saigon</b>' ).change( from: 'b', to: 'em', text: 'hotels' ).run!.should ==
           'Hot <em>hotels</em> in <b>Saigon</b>'
       end
 
       it "should be able to scope changes to specific blocks" do
-        subject.new( fragment: '<div><b>Hotels</b></div><p><b>Hotels</b></p><b>Tents</b>' ).change( from: 'b', to: 'em', scope: 'p' ).run!.should ==
+        cleaner.new( fragment: '<div><b>Hotels</b></div><p><b>Hotels</b></p><b>Tents</b>' ).change( from: 'b', to: 'em', scope: 'p' ).run!.should ==
           '<div><b>Hotels</b></div><p><em>Hotels</em></p><b>Tents</b>'
       end
 
@@ -57,38 +57,38 @@ describe CrazyHarry::Cleaner do
     context "inline tags to blocks" do
 
       it "should be able to change an isolated br to a p" do
-        subject.new( fragment: 'Hotel Details<br />' ).change( from: 'br', to: 'p' ).run!.should == '<p>Hotel Details</p>'
+        cleaner.new( fragment: 'Hotel Details<br />' ).change( from: 'br', to: 'p' ).run!.should == '<p>Hotel Details</p>'
       end
 
       it "should be able to change multiple br tags to a single p" do
-        subject.new( fragment: 'Hotel Details<br /><br /><br />' ).change( from: 'br', to: 'p' ).run!.should == '<p>Hotel Details</p>'
+        cleaner.new( fragment: 'Hotel Details<br /><br /><br />' ).change( from: 'br', to: 'p' ).run!.should == '<p>Hotel Details</p>'
       end
 
       it "should clear unnecessary whitespace when converting line-style to block-style" do
-         subject.new( fragment: 'hotel<br />        hostel      <br />').change( from: 'br', to: 'p').run!.should ==
+         cleaner.new( fragment: 'hotel<br />        hostel      <br />').change( from: 'br', to: 'p').run!.should ==
           '<p>hotel</p><p>hostel</p>'
       end
 
       it "should be able to change all br tags to wrapped p tags" do
-        subject.new( fragment: 'hotel<br /> hostel<br /> tent<br />').change( from: 'br', to: 'p').run!.should ==
+        cleaner.new( fragment: 'hotel<br /> hostel<br /> tent<br />').change( from: 'br', to: 'p').run!.should ==
           '<p>hotel</p><p>hostel</p><p>tent</p>'
       end
 
       it "should be able to change all multiple br tags to wrapped tags" do
-        subject.new( fragment: 'hotel<br /><br /><br/> hostel<br /> tent<br />').change( from: 'br', to: 'p').run!.should ==
+        cleaner.new( fragment: 'hotel<br /><br /><br/> hostel<br /> tent<br />').change( from: 'br', to: 'p').run!.should ==
           '<p>hotel</p><p>hostel</p><p>tent</p>'
       end
 
       it "should be able to convert leading inline tags to block tags" do
-        subject.new( fragment: '<br /><br />hotel').change( from: 'br', to: 'p').run!.should == '<p>hotel</p>'
+        cleaner.new( fragment: '<br /><br />hotel').change( from: 'br', to: 'p').run!.should == '<p>hotel</p>'
       end
 
       it "should ignore stray inline tags if they don't appear relevant to the context" do
-        subject.new( fragment: '<br /><br />hotel <br />hostel<br />').change( from: 'br', to: 'p').run!.should == '<p>hotel</p><p>hostel</p>'
+        cleaner.new( fragment: '<br /><br />hotel <br />hostel<br />').change( from: 'br', to: 'p').run!.should == '<p>hotel</p><p>hostel</p>'
       end
 
       it "should be able to change a br tag to a div" do
-        subject.new( fragment: 'Hotel Details<br />' ).change( from: 'br', to: 'div' ).run!.should == '<div>Hotel Details</div>'
+        cleaner.new( fragment: 'Hotel Details<br />' ).change( from: 'br', to: 'div' ).run!.should == '<div>Hotel Details</div>'
       end
 
     end
