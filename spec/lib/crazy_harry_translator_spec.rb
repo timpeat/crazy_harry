2db1b60302688a58cd68e2a3d989eeca0c0f2319
TA Tyree <tatyree@gmail.com>
2012-06-19 10:17:16 +0100

    Avoid 'subject' code smell.

--- spec/lib/crazy_harry_translator_spec.rb
+++ spec/lib/crazy_harry_translator_spec.rb
@@ -6,11 +6,11 @@ describe CrazyHarry::Translator do
   #
   #   Another ADS suggestion.  Not sure it if it needed for the first cut.
   #   Transform "lodging" to "hotel"
-  
-  subject{ CrazyHarry::Translator }
+
+  let(:translator) { CrazyHarry::Translator }
 
   it "should be able to add an attribute to all tags" do
-    subject.new( fragment: '<b>a</b><br /><h3 class="red">c</h3>' ).translate( add_attributes: { class: 'from-partner'} ).run!.should ==
+    translator.new( fragment: '<b>a</b><br /><h3 class="red">c</h3>' ).translate( add_attributes: { class: 'from-partner'} ).run!.should ==
       '<b class="from-partner">a</b><br class="from-partner"><h3 class="red from-partner">c</h3>'
   end
 
