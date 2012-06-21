2fdccd3b71893a4a7a60e9b4dafd5b9a1efa7a26
TA Tyree <tatyree@gmail.com>
2012-06-21 11:36:58 +0100

    * Change the lib load order.
    * Rename dedupe to default.

--- lib/crazy_harry.rb
+++ lib/crazy_harry.rb
@@ -1,10 +1,23 @@
 require 'loofah'
-Dir[File.expand_path("lib/crazy_harry/**/*.rb")].each {|f| require f}
+
+%w(default base cleaner redactor translator version).each do |load_lib|
+  require_relative "crazy_harry/#{load_lib}"
+end
 
 module CrazyHarry
 
-  def self.fragment(fragment)
-    Base.new(fragment: fragment)  
+  attr_accessor :harry
+
+  class << self
+
+    def fragment(fragment)
+      harry = Base.new(fragment: fragment)  
+    end
+
+    def to_s
+      @harry.to_s
+    end
+
   end
 
 end
