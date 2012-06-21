eb26d339eada66b52283cbcc4f2b79b19681469e
TA Tyree <tatyree@gmail.com>
2012-06-21 15:45:14 +0100

    WIP Translate is duplicating added attributes.

--- spec/integration/crazy_harry_spec.rb
+++ spec/integration/crazy_harry_spec.rb
@@ -12,4 +12,13 @@ describe CrazyHarry do
       .to_s.should == 'Place <p class="partner">Hotel</p><h3 class="partner">Location:</h3>'
   end
 
+  it "should not care about the chain order" do
+    harry.fragment('<script>STEAL COOKIE!</script><em>Place</em><p>Lodging</p><b>Location:</b>')
+      .translate!( from_text: 'Lodging', to_text: 'Hotel', add_attributes: { class: 'partner' } )
+      .redact!( unsafe: true, tags: 'em' )
+      .change!( from: 'b', to: 'h3' )
+      .to_s.should == 'Place <p class="partner">Hotel</p><h3 class="partner">Location:</h3>'
+
+  end
+
 end
