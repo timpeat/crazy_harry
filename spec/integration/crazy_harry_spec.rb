1a6a0bcf3d011e9860f250e2fe3897035df8c8e3
TA Tyree <tatyree@gmail.com>
2012-06-25 14:03:32 +0100

    Removing this spec. Diff is too fragile for this.

--- spec/integration/crazy_harry_spec.rb
+++ spec/integration/crazy_harry_spec.rb
@@ -20,15 +20,4 @@ describe CrazyHarry do
       .to_s.should == 'Place <p class="partner">Hotel</p><h3 class="partner">Location:</h3>'
   end
 
-  it "should work on a real-world example" do
-    bad_fragment        = File.read(File.expand_path('spec/fixtures/golden_nugget.html'))
-    corrected_fragment  = File.read(File.expand_path('spec/fixtures/golden_nugget_corrected.html'))
-
-    harry.fragment(bad_fragment)
-      .redact!( unsafe: true )
-      .change!( from: 'b', to: 'h3' )
-      .change!( from: 'strong', to: 'h3')
-      .to_s.should == corrected_fragment
-  end
-
 end
